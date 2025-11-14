186b40398d50ffa1e07c478f87c6109fef1bd54d
Hamza <hamelrhadiouini@gmail.com>
2025-11-14 17:25:49 +0100

    card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
+import Card from "./Card";
 function App() {
   
   return (
     <>
-    <Header></Header>
-    <Food/>
-    <Footer></Footer>
+     <Card />
     </>
    
 
