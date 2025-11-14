ac16ec99110aeb4f6ae29028acb80dbaa5d814c2
Hamza <hamelrhadiouini@gmail.com>
2025-11-14 17:01:40 +0100

    Introduction to components

---
+++ src/Food.jsx
@@ -0,0 +1,18 @@
+
+
+
+function Food() {
+    const food1 = "Pizza";
+    const food2 = "Burger";
+
+  return (
+  <ul>
+    <li>{food1}</li>
+    <li>{food2.toUpperCase()}</li>
+    <li>Tacos</li>
+  </ul>
+
+
+  );
+}
+export default Food;
---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+function Footer() {
+  return (
+
+    <footer>
+      <p>© {new Date().getFullYear()} My React App. All rights reserved.</p>
+    </footer>
+
+  )
+}
+
+
+  export default Footer;
