bb0373c0bf9fd186d2431cf053edb0ddefe9e34e
Hamza <hamelrhadiouini@gmail.com>
2025-11-15 13:49:37 +0100

    learn Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import List from "./List"
 
-import Student from "./Student"
 function App() {
   
   return (
     <>
-       <Student name="Alice"  age="20" grade="A"/>
-        <Student name="Bob"  age="22" grade="B"/>
+       <List />
        
     </>
    
